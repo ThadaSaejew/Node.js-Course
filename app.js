effe4e1277cc7f0b59478bb017168762a2607e9d
ThadaSaejew <143861007+ThadaSaejew@users.noreply.github.com>
2023-09-02 15:07:44 +0700

    EP19

--- app.js
+++ app.js
@@ -3,20 +3,36 @@ const express = require('express');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
-
+const productRouter= express.Router();
+const products = require("./data/products.json")
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT;
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname,"/public/")));
 
+app.set("views","./src/views");
+app.set("view engine","ejs");
+
+productRouter.route("/").get((req,res)=>{
+    res.render("products",{
+        products:[
+            {productTiltle: "A 1 ", productDescription: "A 1 ",productPrice: 10},
+            {productTiltle: "A 1 ", productDescription: "A 1 ",productPrice: 20},
+            {productTiltle: "A 1 ", productDescription: "A 1 ",productPrice: 30},
+            {productTiltle: "A 1 ", productDescription: "A 1 ",productPrice: 40},
+        ],
+    });
+});
+app.use("/products",productRouter)
+
 app.get("/",(req,res) =>{
 
-    res.send("Hello World1");
+    res.render('index',{username:'Book' , customers:[ "thada" , "book" , "saejew"]});
 
 })
 
-app.listen(port,() =>{
-    debug("Listening on port %d",port);
+app.listen(PORT,() =>{
+    debug("Listening on port %d",PORT);
 
 })
